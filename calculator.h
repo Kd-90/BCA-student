bd65c5b2afded93dd12371f4ce62c1de93f49677
K D - 90 <krishnadixit3012@gmail.com>
2025-07-21 21:43:36 +0530

    calculator.h

--- calculator.h
+++ calculator.h
@@ -1,10 +1,11 @@
-#ifndef CALCULATOR_H
-#define CALCULATOR_H
+#include<iostream>
 
 // Function declarations for arithmetic operations
-int add(int a, int b);
-int subtract(int a, int b);
-int multiply(int a, int b);
-double divide(int a, int b); // Use double for division result
-
-#endif // CALCULATOR_H
+int add(int a, int b) {
+  return a+b;
+int subtract(int a, int b) {
+  return a-b;
+int multiply(int a, int b) {
+  return a*b;
+int divide(int a, int b) {
+  return a/b;
